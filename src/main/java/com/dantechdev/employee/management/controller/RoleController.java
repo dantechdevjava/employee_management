9d9cab7595634e2177566275b6b9d99de7c184d0
DanTechDev <dantechdev@outlook.es>
2025-12-05 12:21:41 +0100

    fix SecurityConfig and RoleController

--- src/main/java/com/dantechdev/employee/management/controller/RoleController.java
+++ src/main/java/com/dantechdev/employee/management/controller/RoleController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/roles")
+@RequestMapping("/api/roles")
 public class RoleController {
     @Autowired
     private RoleService roleService;
